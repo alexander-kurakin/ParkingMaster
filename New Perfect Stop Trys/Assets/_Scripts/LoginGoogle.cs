f137726bb22b30087e009854d6947c55d000044d
Kurakin Alexander <kurakin.alexander.a@gmail.com>
2018-04-09 21:29:26 +0300

    Initial commit
    Car game big commit

---
+++ New Perfect Stop Trys/Assets/_Scripts/LoginGoogle.cs
@@ -0,0 +1,65 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginGoogle : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().EnableSavedGames().Build();
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+
+        PlayGamesPlatform.Instance.Authenticate(SignInCallBack, true);
+
+        if (!Social.localUser.authenticated)
+            PlayGamesPlatform.Instance.Authenticate(SignInCallBack, false);
+
+        if (Social.localUser.authenticated)
+        WriteResult();
+
+
+    }
+
+    private void SignInCallBack(bool success)
+    {
+        if (success)
+        {
+            Debug.Log  ("Signed in as: " + Social.localUser.userName);
+        }
+        else
+        { 
+                Debug.Log( "Not signed in");
+            
+        }
+    }
+
+    public void ShowAchievements()
+    {
+        ((PlayGamesPlatform)Social.Active).ShowAchievementsUI();
+    }
+
+    public void ShowLeaderboard()
+    {
+            ((PlayGamesPlatform)Social.Active).ShowLeaderboardUI(GPGSIds.leaderboard_top_parked_cars);
+    }
+
+    public void WriteResult()
+    {
+        if (Social.localUser.authenticated)
+        {
+            
+            Social.ReportScore(PlayerPrefs.GetInt("Score"),
+                GPGSIds.leaderboard_top_parked_cars,
+                (bool success) =>
+                {
+                    Debug.Log ( "Success!" );
+                });
+        }
+    }
+
+}
